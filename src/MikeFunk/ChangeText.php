234e8d96e27aabbeb5a4e1bd3e36c15ce6b8aeac
Mike Funk <mfunk@christianpublishing.com>
2013-03-03 15:10:57 -0500

    re-added src

---
+++ src/MikeFunk/ChangeText.php
@@ -0,0 +1,38 @@
+<?php
+namespace MikeFunk;
+
+use MikeFunk\ChangeTextException;
+
+/**
+ * just changes spaces with dashes.
+ *
+ * @package ExampleProject
+ * @author Mike Funk <mfunk@christianpublishing.com>
+ * @copyright 2013 Xulon Press, Inc. All Rights Reserved.
+ */
+
+class ChangeText
+{
+    /**
+     * get dependencies
+     */
+    public function __construct()
+    {
+        require_once(__DIR__.'/../../vendor/autoload.php');
+        require_once(__DIR__.'/Exceptions.php');
+    }
+
+    /**
+     * just changes spaces with dashes.
+     *
+     * @param string $text the input text with spaces
+     * @return string the changed text with dashes
+     */
+    public function change($text)
+    {
+        if (strpos($text, '!!') !== false) {
+            throw new ChangeTextException('Text has double bang');
+        }
+        return str_replace(' ', '-', $text);
+    }
+}
